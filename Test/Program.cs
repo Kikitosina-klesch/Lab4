4c9bdbf3b8c0514d68bc95e302128d77a10047f7
Kikita <nik-fadeev-2000@mail.ru>
2020-11-21 23:31:18 +0700

    fjfjfjfjfjaaaaaaaaaaaaaaaaaaa

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,7 +7,6 @@ namespace Test
         static void Main(string[] args)
         {
             double res1 = A + B;
-
             Console.WriteLine("Результатом сложения А и B будет" + Convert.ToString(res1));
         }
     }
